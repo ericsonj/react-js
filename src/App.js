baab2adbeefa3ec3f05bb166531d0de677c5a07f
Ericson <eric.surix@gmail.com>
2021-06-02 16:23:07 -0300

    Test for framework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Box from "@material-ui/core/Box"
+import Button from "@material-ui/core/Button"
+import Grid from  "@material-ui/core/Grid"
+
+class JButton extends React.Component {
+  render() {
+    return (
+      <Button {...this.props}>
+        {this.props.label}
+      </Button>
+    )
+  }
+}
+
+function Elem(objs) {
+  return objs.map((objs) => <MyComponent {...objs} />);
+}
+
+class MyComponent extends React.Component {
+
+  components = {
+    jbox: Box,
+    jbutton: JButton,
+    jgrid: Grid
+  };
+
+  render() {
+    const TagName = this.components[this.props.tag || 'jbutton'];
+    return (<TagName  {...this.props.attr} >{this.props.children ? Elem(this.props.children ) : ''}</TagName>)
+  }
+}
 
 function App() {
+
+  const buttons = [
+    {
+      tag: "jgrid",
+      attr: {
+        container: true,
+        spacing: 3
+      },
+      children: [
+        {
+          tag: "jgrid",
+          attr: {
+            item: true,
+            xs: 6
+          },
+          children: [
+            {
+              tag: "jbutton",
+              attr: {
+                color: 'secondary',
+                label: 'Clik Me',
+                variant: "contained"
+              }
+            }
+          ]
+        },
+        {
+          tag: "jgrid",
+          attr: {
+            item: true,
+            xs: 6
+          },
+          children: [
+            {
+              tag: "jbutton",
+              attr: {
+                color: 'primary',
+                label: 'Clik Me',
+                variant: "contained"
+              }
+            }
+          ]
+        },
+      ]
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {Elem(buttons)}
+    </>
   );
 }
 
